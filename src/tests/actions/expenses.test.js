72e44c13d58bab3ccf83671a4804a8bfa132b9a9
selih.design@gmail.com <loalegba@gmail.com>
2018-06-21 14:41:06 +0200

    Test cases for expense actions added.

---
+++ src/tests/actions/expenses.test.js
@@ -0,0 +1,51 @@
+import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+
+test('should setup remove expense action object', () => {
+    const action = removeExpense({ id: '123abc' });
+    expect(action).toEqual({
+        type: 'REMOVE_EXPENSE',
+        id: '123abc'
+    });
+});
+
+test('should setup the edit expense action object', () => {
+    const action = editExpense('test123', { note: 'New value' });
+    expect(action).toEqual({
+        type: 'EDIT_EXPENSE',
+        id: 'test123',
+        updates: {
+            note: 'New value'
+        }
+    });
+});
+
+test('should setup add expense action object with provided values', () => {
+    const expenseData = {
+        description: 'Rent',
+        amount: 109500,
+        createdAt: 1000,
+        note: 'this was last months rent'
+    };
+    const action = addExpense(expenseData);
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            ...expenseData,
+            id: expect.any(String)
+        }
+    });
+});
+
+test('should setup add expense action object with default values', () => {
+    const action = addExpense();
+    expect(action).toEqual({
+        type: 'ADD_EXPENSE',
+        expense: {
+            id: expect.any(String),
+            description: '',
+            note: '',
+            amount: 0,
+            createdAt: 0
+        }
+    });
+});
